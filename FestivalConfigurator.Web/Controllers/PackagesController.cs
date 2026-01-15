20b92927f2a469438163d4d7bc5d1cea5ffada6f
kidfles <166709765+kidfles@users.noreply.github.com>
2026-01-15 09:24:17 +0100

    -

--- FestivalConfigurator.Web/Controllers/PackagesController.cs
+++ FestivalConfigurator.Web/Controllers/PackagesController.cs
@@ -149,8 +149,6 @@ namespace FestivalConfigurator.Web.Controllers
         }
 
         // POST: Packages/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         // Saves the changes you made to the package.
